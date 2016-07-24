004992f1352225a70337db4714f6532340461995
Diego Martin Vazquez Nanini <diego@vazqueznanini.com>
2016-07-24 13:52:30 +0100

    WIP excersesis

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,11 +1,12 @@
 package quickcheck
 
 import common._
-
 import org.scalacheck._
 import Arbitrary._
 import Gen._
 import Prop._
+
+import scala.annotation.tailrec
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
@@ -15,6 +16,13 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   } yield insert(elem, m)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+  @tailrec
+  private def isSorted(h: H): Boolean = {
+    if (isEmpty(h)) true
+    else if (isEmpty(deleteMin(h))) true
+    else { if (findMin(h) > findMin(deleteMin(h))) false else isSorted(deleteMin(deleteMin(h))) }
+  }
 
   property("min1") = forAll { a: Int =>
     val h = insert(a, empty)
@@ -26,28 +34,23 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
-  property("suggestion1") = forAll { (x: Int, y: Int) =>
+  property("hint1") = forAll { (x: Int, y: Int) =>
     val m = insert(y, insert(x, empty))
-    findMin(m) == (if (x < y) y else x)
+    findMin(m) == ord.min(x, y)
   }
 
-  property("suggestion2") = forAll { a: Int =>
+  property("hint2") = forAll { a: Int =>
     val m = insert(a, empty)
     deleteMin(m) == empty
   }
 
-  property("suggestion3") = forAll { (h: H) =>
-    def sorted(h: H, min: A): H = {
-      h
-    }
-
-    //findMin(h) < findMin(deleteMin(h)) < findMin(deleteMin(deleteMin(h)))
+  property("hint3") = forAll { (h: H) =>
+    isSorted(h)
   }
 
-  property("suggestion4") = forAll { (h: H, j: H) =>
+  property("hint4") = forAll { (h: H, j: H) =>
     val minMerge = findMin(meld(h, j))
     findMin(h) == minMerge || findMin(j) == minMerge
   }
-
 
 }
